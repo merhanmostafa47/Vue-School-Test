2096aee2a10b77ce14d4e7cb037d06f260bd0a55
Merhan Mostafa <merhanmostafa47@gmail.com>
2024-03-29 23:17:19 +0200

    Add  default font family configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,31 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  // ============ Project Css  ============ //
   css: ['~/assets/css/main.css'],
 
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+
+  // ============ Modules Registeration ============ //
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts'
+  ],
+
+  // ============ Modules Configurations ============ //
+
+  tailwindcss: {
+    configPath: 'tailwind.config',
+  },
+
+  googleFonts: {
+    fontsDir: '~/assets/fonts',
+    base64: false,
+
+    families: {
+      Rubik: {
+        wght: [400, 500, 600, 700],
+      },
     },
+    display: 'swap',
   },
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: [
     "./components/**/*.{js,vue,ts}",
@@ -9,7 +10,9 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+    },
   },
   plugins: [],
 };

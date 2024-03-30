a6bf989bcaa137b40c95ea3ac78f6167a117f6af
Merhan Mostafa <merhanmostafa47@gmail.com>
2024-03-30 04:03:33 +0200

    Add seo tags

--- app.vue
+++ app.vue
@@ -1,3 +1,24 @@
+<script setup>
+useSeoMeta({
+  title: "Vue School",
+  ogTitle: "Vue School",
+  description:
+    "School is the #1 training resource for Vue.js and has provided the most comprehensive Vue.js video library in the world to over 140,000 developers.",
+  ogDescription:
+    "School is the #1 training resource for Vue.js and has provided the most comprehensive Vue.js video library in the world to over 140,000 developers.",
+  ogImage: "/logo.svg",
+  twitterCard: "summary_large_image",
+  twitterTitle: "Vue School.io",
+  twitterDescription:
+    "School is the #1 training resource for Vue.js and has provided the most comprehensive Vue.js video library in the world to over 140,000 developers.",
+  twitterImage: "/logo.svg",
+});
+
+useHead({
+  htmlAttrs: { lang: "en" },
+  link: [{ rel: "canonical", href: "https://vue-school.com/" }],
+});
+</script>
 <template>
   <NuxtLayout>
     <NuxtPage />

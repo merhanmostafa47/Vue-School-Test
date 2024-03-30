ae8250db4204485ca6cc6027c6f06652f5336057
Merhan Mostafa <merhanmostafa47@gmail.com>
2024-03-30 13:16:10 +0200

    Update seo tags

--- app.vue
+++ app.vue
@@ -1,17 +1,23 @@
 <script setup>
 useSeoMeta({
   title: "Vue School",
-  ogTitle: "Vue School",
   description:
     "School is the #1 training resource for Vue.js and has provided the most comprehensive Vue.js video library in the world to over 140,000 developers.",
+
+  // OpenGraph Meta Tags
+  ogTitle: "Vue School",
   ogDescription:
     "School is the #1 training resource for Vue.js and has provided the most comprehensive Vue.js video library in the world to over 140,000 developers.",
-  ogImage: "/logo.svg",
+  ogImage: "/images/logo.svg",
+  ogUrl: "https://vue-school.com/",
+  ogType: "website",
+
+  // Twitter Meta Tags
   twitterCard: "summary_large_image",
   twitterTitle: "Vue School.io",
   twitterDescription:
     "School is the #1 training resource for Vue.js and has provided the most comprehensive Vue.js video library in the world to over 140,000 developers.",
-  twitterImage: "/logo.svg",
+  twitterImage: "/images/logo.svg",
 });
 
 useHead({

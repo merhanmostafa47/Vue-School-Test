503a92fd1ae4d8c700aebb781df0f5357ecdfe7b
Merhan Mostafa <merhanmostafa47@gmail.com>
2024-03-30 04:04:01 +0200

    Add typescript configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,7 +63,12 @@ export default defineNuxtConfig({
     mirror: false, // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
   },
+
   // ============ General Configurations ============ //
+
+  typescript: {
+    shim: false,
+  },
 
   components: {
     dirs: [
